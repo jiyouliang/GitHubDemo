4787569ac1e729aeadfa323cfa47b762bb7da05e
jiyouliang <jeyouliang@gmail.com>
2019-11-08 20:00:14 +0800

    初始最热页面顶部导航

--- js/page/PopularPage.tsx
+++ js/page/PopularPage.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
 
 export default class PopularPage extends React.Component {
+  // 顶部导航
+  _initTopTab() {
+    return createAppContainer(
+      createMaterialTopTabNavigator({
+        AndroidTab: {
+          screen: AndroidTab,
+          navigationOptions: {
+            title: 'Android',
+          },
+        },
+        IosTab: {
+          screen: IosTab,
+          navigationOptions: {
+            title: 'IOS',
+          },
+        },
+      }),
+    );
+  }
+
+  public render() {
+    const TopTab = this._initTopTab();
+    return (
+      <View style={styles.container}>
+        <TopTab />
+      </View>
+    );
+  }
+}
+
+class AndroidTab extends React.Component {
   public render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>PopularPage</Text>
+        <Text style={styles.title}>Android</Text>
+      </View>
+    );
+  }
+}
+
+class IosTab extends React.Component {
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>IOS</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    title:{
-        fontSize:20,
-        color:'#020202',
-    }
+  container: {
+    flex: 1,
+    // marginTop:30
+  },
+  title: {
+    fontSize: 20,
+    color: '#020202',
+  },
 });
-
