e7bf977e5477e85fae03bc88de40988872d4fdbd
jiyouliang <jeyouliang@gmail.com>
2019-11-08 15:38:57 +0800

    欢迎页

--- js/page/WelcomePage.tsx
+++ js/page/WelcomePage.tsx
@@ -9,6 +9,17 @@ import {View, Text, StyleSheet} from 'react-native';
  * @extends {React.Component}
  */
 export default class WelcomePage extends React.Component {
+    timer: NodeJS.Timeout | undefined;
+  /**
+   *页面加载完成
+   *
+   */
+  public componentDidMount() {
+      this.timer = setTimeout(() => {
+          // TODO 进入主页
+      }, 2000);
+  }
+
   public render() {
     return (
       <View style={styles.container}>
@@ -16,16 +27,21 @@ export default class WelcomePage extends React.Component {
       </View>
     );
   }
+
+  // 页面销毁，取消计时器，避免内存溢出
+  public componentWillUnmount(){
+    this.timer && clearTimeout(this.timer)
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    title:{
-        fontSize:20,
-        color:'black',
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: 'black',
+  },
 });
